dc9cbc3b662df84294bca982400b6da7ffcaacb0
Rohan Lather <rohanlather2010@gmail.com>
2024-07-11 00:38:59 +0530

    first commit

---
+++ packages/server/database.sql
@@ -0,0 +1,11 @@
+CREATE TABLE users(
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(28) NOT NULL UNIQUE,
+    passhash VARCHAR NOT NULL,
+    userid VARCHAR NOT NULL UNIQUE
+);
+
+INSERT INTO users(username, passhash) values($1,$2);
+
+ALTER TABLE users
+ADD COLUMN userid VARCHAR(50)  UNIQUE;
